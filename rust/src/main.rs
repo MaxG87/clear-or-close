4d2606e5b425cfb0430ef7dd4811adf8beca2d47
Max Görner <5477952+MaxG87@users.noreply.github.com>
2021-06-10 00:36:52 +0200

    Remove all functions written for training only

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,29 +2,6 @@ use rand::distributions::*;
 use rand::thread_rng;
 
 
-fn fac(n: u64) -> u64 {
-    if n == 1 {
-        1
-    } else { n*fac(n-1) }
-
-}
-
-
-fn flip_coin() -> bool {
-    let d = Bernoulli::new(0.5).unwrap();
-    let v = d.sample(&mut thread_rng());
-    return v
-}
-
-fn choose_weighted() -> char {
-    let choices = ['a', 'b', 'c'];
-    let weights = [1E10, 5E9, 5E9];
-    let dist = WeightedIndex::new(&weights).unwrap();
-    let mut rng = thread_rng();
-    choices[dist.sample(&mut rng)]
-}
-
-
 /// Sample from Hypergeometric Distribution
 ///
 /// This function takes a vector of counts and draws k elements from those
@@ -46,10 +23,6 @@ fn sample_hypergeometric(counts: Vec<u64>, k: u64) -> Vec<u64> {
 
 
 fn main() {
-    println!("The faculty of {} is {}", 5, fac(5));
-    println!("The result of the coin flip is {}", flip_coin());
-    println!("The result of the weighted sampling is {}", choose_weighted());
-
     let n_elligible_voters = 46_500_000;
     let turnout: f64 = 0.7221;
     let n_voters: u32 = (n_elligible_voters as f64 * turnout).floor() as u32;
