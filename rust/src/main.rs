ae34c8979b9f2c964e3ba84b96a6f28d07b5a95b
Max Görner <5477952+MaxG87@users.noreply.github.com>
2021-06-09 22:09:00 +0200

    Add function to choose from weighted

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,5 @@
 use rand::distributions::*;
+use rand::thread_rng;
 
 
 fn fac(n: u64) -> u64 {
@@ -11,12 +12,21 @@ fn fac(n: u64) -> u64 {
 
 fn flip_coin() -> bool {
     let d = Bernoulli::new(0.5).unwrap();
-    let v = d.sample(&mut rand::thread_rng());
+    let v = d.sample(&mut thread_rng());
     return v
+}
+
+fn choose_weighted() -> char {
+    let choices = ['a', 'b', 'c'];
+    let weights = [1E10, 5E9, 5E9];
+    let dist = WeightedIndex::new(&weights).unwrap();
+    let mut rng = thread_rng();
+    choices[dist.sample(&mut rng)]
 }
 
 
 fn main() {
     println!("The faculty of {} is {}", 5, fac(5));
     println!("The result of the coin flip is {}", flip_coin());
+    println!("The result of the weighted sampling is {}", choose_weighted());
 }
