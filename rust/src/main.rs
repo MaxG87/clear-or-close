fd480f4dcb6bc0f1fdd08ae0525bc94cc3f87be7
Max Görner <5477952+MaxG87@users.noreply.github.com>
2021-06-09 21:54:48 +0200

    Add flipping a fair coin as sampling example

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,22 @@
+use rand::distributions::*;
+
+
 fn fac(n: u64) -> u64 {
     if n == 1 {
         1
     } else { n*fac(n-1) }
 
 }
+
+
+fn flip_coin() -> bool {
+    let d = Bernoulli::new(0.5).unwrap();
+    let v = d.sample(&mut rand::thread_rng());
+    return v
+}
+
+
 fn main() {
     println!("The faculty of {} is {}", 5, fac(5));
+    println!("The result of the coin flip is {}", flip_coin());
 }
